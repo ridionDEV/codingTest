348d3f983516c78a0fc9164ff0e2354e99072f8e
엔카리디언 <ridion0320@gmail.com>
2025-09-04 11:26:45 +0900

    ddsadas

---
+++ codingtest/Algorithm/1082.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    int n,m;
+    cin >> n;
+    vector<int> A(n);
+    string result ="";
+    for(int i=0;i<n;i++){
+        cin >> A[i];
+    }
+    cin >> m;
+
+    for(int i=n-1;i<=0;i--){
+        if(m > )
+    }
+
+
+    return 0;
+}
